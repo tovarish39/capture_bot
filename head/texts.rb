de9e389a30e0f439c90c61906efbb90de1564cd6
Alexey3934 <gorrus39@mial.ru>
2023-03-06 15:46:31 +0200

    q

--- head/texts.rb
+++ head/texts.rb
@@ -44,5 +44,5 @@ B_successs = {
     ru:"Капча пройдена успешно!\nТвоя ссылка для вступления:\nОна действительна в течение 1 минуты",
     en:"Done! Congratulations!\nThat's your link to join the chat:\nIt's good for 1 minute",
     es:"¡Exitoso!\nEse es su enlace para unirse:\nEs válido por 1 minuto",
-    cn:"验证码成功通过！/n您的入口链接:\n1分钟有效"
+    cn:"验证码成功通过！\n您的入口链接:\n1分钟有效"
 }
